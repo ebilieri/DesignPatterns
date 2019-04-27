d0e3ca10c6b79f9ad04443cd0a0e64f194dc403a
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-04-27 13:51:58 -0300

    Factoty Method

---
+++ FactoryMethod/FactotyMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+   public class FactotyMethod
+    {
+        public IPersonagem EscolherPersonagem(string personagem)
+        {
+            switch (personagem.ToUpper())
+            {
+                case "LIU KANG": return new LiuKang();
+                case "SCORPION": return new Scorpion();
+                case "SUBZERO": return new SubZero();
+                default:return null;
+            }
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactotyMethod fm = new FactotyMethod();
+
+            Console.WriteLine("Liu Kang | SubZero | Scopion");
+            Console.WriteLine();
+
+            Console.WriteLine("Escolha seu personagem");
+            string escolha = Console.ReadLine();
+
+            IPersonagem personagem = fm.EscolherPersonagem(escolha);
+            Console.WriteLine();
+            Console.Write("Você vai jogar com: ");
+
+            if (personagem != null)
+                personagem.Escolhido();
+            else
+                Console.WriteLine("Personagem invalido");
+
+            Console.ReadKey();
+        }
+    }
+}
