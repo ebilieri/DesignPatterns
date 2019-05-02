2c0a7e40b7d672db40daec283885fefabccd963c
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-05-01 21:50:06 -0300

    AbstractFactory

---
+++ AbstractFactory/CarroPopularFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    public class CarroPopularFactory : CarroFactory
+    {
+        public override Roda MontarRoda()
+        {
+            return new RodaSimples();
+        }
+
+        public override Som MontarSom()
+        {
+            return new CDPlayer();
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = ExecutaAbstractFactory.MontarCarro("luxo");
+            Console.WriteLine(c1.Roda);
+            Carro c2 = ExecutaAbstractFactory.MontarCarro("popular");
+            Console.WriteLine(c2.Roda);
+
+            Console.ReadKey();
+        }
+    }
+}
